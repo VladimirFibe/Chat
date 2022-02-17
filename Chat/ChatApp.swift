8a2e4ccbed5d8e82cdd1aad5e22a3b6130ebdfde
Vladimir Fibe <motiw@icloud.com>
2022-02-17 20:20:42 +0600

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Vladimir Fibe on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
