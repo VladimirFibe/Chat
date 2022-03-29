1801b04660903206016ba3ec12caf99bdec09d70
Vladimir Fibe <motiw@icloud.com>
2022-03-29 13:20:50 +0600

    Start with Stephan

---
+++ Chat/View/Authentication/AuthView.swift
@@ -0,0 +1,22 @@
+//
+//  AuthView.swift
+//  Chat
+//
+//  Created by Владимир Файб on 29.03.2022.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+      VStack {
+        Text("Hello, Chat!")
+      }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
