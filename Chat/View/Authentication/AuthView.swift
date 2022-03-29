608ee9efb46e8c6093b826f472077cd897f2779b
Vladimir Fibe <motiw@icloud.com>
2022-03-29 14:03:51 +0600

    add AuthView

--- Chat/View/Authentication/AuthView.swift
+++ Chat/View/Authentication/AuthView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct AuthView: View {
-    var body: some View {
-      VStack {
-        Text("Hello, Chat!")
+  var body: some View {
+    NavigationView {
+      VStack(alignment: .leading) {
+        Text("Hello")
+          .font(.largeTitle)
+          .bold()
+        
+        Text("Welcome Back")
+          .font(.largeTitle)
+          .bold()
+          .foregroundColor(.blue)
       }
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+      .padding()
+      .navigationBarHidden(true)
     }
+  }
 }
 
 struct AuthView_Previews: PreviewProvider {
-    static var previews: some View {
-        AuthView()
-    }
+  static var previews: some View {
+    AuthView()
+  }
 }
